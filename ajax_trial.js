74e0220d3e194919c3149df2475c76b94df3a5b1
Rupan007 <94926715+Rupan007@users.noreply.github.com>
2021-11-29 19:03:45 +0530

    Update ajax_trial.js

--- ajax_trial.js
+++ ajax_trial.js
@@ -1,6 +1,1 @@
-<div>ATKMB 4 FT 2 KBFC</div>
-<div>H. Boumous 3’,37’ &nbsp;&nbsp; Sahal 25’</div>
-<div>R. Krishna 27' &nbsp;&nbsp; Diaz 76’</div>
-<div>L. Colaco 55’</div>
-<div>Man of the Match - Hugo Boumous</div>
-<div>ATKMOHUNBAGAN won the match</div>
+<div>Hey there I will be busy with my exams these days so I will discontinue this and will try to resolve it after 15th Wednesday, December, 2021.</div> 
