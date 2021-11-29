7cde152b1ec53839e130777b2cd5f3ffab0835a5
Rupan007 <94926715+Rupan007@users.noreply.github.com>
2021-11-29 20:13:38 +0530

    Create score-control.js

---
+++ score-control.js
@@ -0,0 +1,7 @@
+'<div class="logo"><img src="https://www.indiansuperleague.com/static-resources/images/clubs/small/reverse/1874.png" alt="atkmb"></div>
+								 			<div>0</div>
+								 			<div class="minutes-contain">
+								 						-<div class="minutes">FT</div>-
+								 			</div>
+								 			<div>0</div>
+								 			<div class="logo"><img src="https://www.indiansuperleague.com/static-resources/images/clubs/small/reverse/498.png" alt="kbfc"></div>'
