f52d4047a9721a34aafe6fcf3c9aed11855ce27a
Rupan007 <94926715+Rupan007@users.noreply.github.com>
2021-11-29 18:05:01 +0530

    Update ajax_trial.js

--- ajax_trial.js
+++ ajax_trial.js
@@ -1,3 +1,4 @@
 <div>ATKMB 3 - 1 KBFC</div>
-<div>Boumous 3’,37’    Sahal 25’</div>
-<div>Krishna 27’     </div>
+<div>H. Boumous 3’,37’ &nbsp;&nbsp;Sahal 25’</div>
+<div>R. Krishna 27'</div>
+<div>L. Colaco 55’</div>
